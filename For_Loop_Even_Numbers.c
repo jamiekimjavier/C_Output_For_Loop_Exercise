d6ab4af68489884ccc2402632fbb869ab8701beb
jamiekimjavier <jamiekimjavier@gmail.com>
2024-11-12 00:21:36 +0800

    Add files via upload

---
+++ For_Loop_Even_Numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Loop from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i); 
+        }
+        
+    }
+        printf("\n");
+    
+    return 0;
+}
